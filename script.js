bd930b2448e645b31087560a0e0d3a79bd7cb598
Kourken Kasbarian <kourkenk@outlook.com>
2018-01-08 01:28:10 +0400

    Added Notifications Toggle Function

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ $(document).ready(function(){
 
 /* End Apps List Toggle */
 
+/* Notifications Toggle */
+
+$(document).ready(function(){
+    $(".notification-btn").click(function(){
+        $("#notif").toggle();
+    });
+});
+
+/* End Notifications Toggle */
